8a16056ad144ffb09e4c5c2d03d52d78c2f62d81
Weston Schutt <wtschutt@gmail.com>
2023-04-12 16:11:17 -0600

    Add challenge answers and self-evaluation

--- each.rb
+++ each.rb
@@ -6,8 +6,23 @@
 # Use the each method to print out a complete sentence stating the weather using the Array below:
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+# Calls the .each method on weather_options array, and declares variable for each element as weather_option
+weather_options.each do |weather_option|
+# Iterates throught he array, puts-ing the question with each element.
+  puts "Do you think tomorrow's weather will be #{weather_option}?"
+end
+
+
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+
+# Calls the .each method on words array, and declares variable word for each element.
+words.each do |word|
+# Iterates through each word to evaluate if number of characters is exactly 3 characters in length, then prints the word if true
+  if word.length == 3
+    puts word
+  end
+end
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
--- self-evaluation.md
+++ self-evaluation.md
@@ -1,6 +1,5 @@
 # Self Evaluation
 
-- How might the `each` method be useful?
-- What do you still need to practice or learn? How will you do that?
-- What worked well for you in terms of your approach on this
-assignment?
+- **How might the `.each` method be useful?** The `.each` method seems extremely useful for automating tasks or functions.
+- **What do you still need to practice or learn? How will you do that?** I know that sometimes it is useful to have nested arrays, so I am interested to learn use cases and steps to interate through those as well.
+- **What worked well for you in terms of your approach on this assignment?** I think that I had a mentality to just-try-it and see what happens. I think this helped me to truly understand what works and what does not work, with less time spent in a google rabbit hole.
